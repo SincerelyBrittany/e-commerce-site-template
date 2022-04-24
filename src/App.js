0c480dec8028dd85ec7321bd1b115a6579a7ebff
Sincerely Brittany <sincerelybr@gmail.com>
2022-04-23 23:33:22 -0400

    updates navigation to be  its own component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React from "react";
 import "./App.css";
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
+import Navigation from "./Components/Navigation/navigation";
 import HomePage from "./Components/Pages/Homepage/homepage";
-
-const Navigation = () => {
-  return (
-    <div>
-      <h1> THis is navigation </h1>
-      <Outlet />
-    </div>
-  );
-};
 
 const App = () => {
   return (
