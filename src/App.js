7024c7f077dfa9a5bd61567b0b20f58d9945fb47
Sincerely Brittany <sincerelybr@gmail.com>
2022-04-23 23:21:35 -0400

    adds routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from "react";
 import "./App.css";
+import { Routes, Route, Outlet } from "react-router-dom";
 
 import HomePage from "./Components/Pages/Homepage/homepage";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <HomePage />
-      </div>
-    );
-  }
-}
+const Navigation = () => {
+  return (
+    <div>
+      <h1> THis is navigation </h1>
+      <Outlet />
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        {/* <Route index element={<Home />} /> */}
+        <Route index element={<HomePage />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
