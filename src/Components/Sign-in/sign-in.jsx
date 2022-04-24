cea455dff49fd855a218f3891382e5da3a726062
Sincerely Brittany <sincerelybr@gmail.com>
2022-04-24 13:53:46 -0400

    signInWithGooglePopup example

--- src/Components/Sign-in/sign-in.jsx
+++ src/Components/Sign-in/sign-in.jsx
@@ -1,4 +1,12 @@
+import {
+    signInWithGooglePopup
+  } from '../../utils/firebase/firebase';
+
 const SignIn = () => {
+    const logGoogleUser = async () => {
+        const response = await signInWithGooglePopup
+        console.log(response)
+    }
     return(
 <div>
     <h1> Sign in </h1>
--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -3,6 +3,15 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAHZo4VZw0fI3ZXqJxif1jTo46v9VsGgfQ",
@@ -15,3 +24,15 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
