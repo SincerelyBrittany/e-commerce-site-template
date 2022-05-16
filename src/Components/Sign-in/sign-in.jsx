be4ea80517f100804afda29cf347828b054fe9dc
Sincerely Brittany <sincerelybr@gmail.com>
2022-05-15 20:57:42 -0400

    adds login =withgoogle redirect example

--- src/Components/Sign-in/sign-in.jsx
+++ src/Components/Sign-in/sign-in.jsx
@@ -10,6 +10,9 @@ const SignIn = () => {
     useEffect(async()=> {
        const response = await getRedirectResult(auth)
        console.log(response)
+       if(response){
+        const userDocRef = await createUserDocumentFromAuth(response.user)
+       }
     },[])
     const logGoogleUser = async () => {
         // const response = await signInWithGooglePopup()
